cf65841e04fa2a3e15d9216b83746d68b4d3698a
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-08-06 12:29:27 +0530

    digital_clock

---
+++ digital_clock/script.js
@@ -0,0 +1,24 @@
+function updateClock() {
+      const clockElement = document.getElementById('clock');
+      const dateElement = document.getElementById('date');
+      const now = new Date();
+      
+      
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      const seconds = String(now.getSeconds()).padStart(2, '0');
+
+
+      const day = String(now.getDate()).padStart(2, '0');
+      const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+      const year = now.getFullYear();
+  
+      clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+      dateElement.textContent = `${day}/${month}/${year}`;
+  }
+  
+  setInterval(updateClock, 1000);
+  
+  // Initial call to display the clock immediately on load
+  updateClock();
+  
